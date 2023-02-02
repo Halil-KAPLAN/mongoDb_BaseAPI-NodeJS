87e9a240771f36ef068541cf8499711565440d7a
Halil KAPLAN <halil-kaplan@windowslive.com>
2023-02-03 00:10:16 +0300

    login validaiton

--- src/middlewares/validations/auth.validation.js
+++ src/middlewares/validations/auth.validation.js
@@ -56,6 +56,44 @@ class authValidation {
     }
     next();
   };
+
+  static login = async (req, res, next) => {
+    try {
+      await joi
+        .object({
+          email: joi
+            .string()
+            .email()
+            .trim()
+            .min(3)
+            .max(100)
+            .required()
+            .messages({
+              "string.base": "Email field must be valid text !",
+              "string.empty": "Email field cannot be empty !",
+              "string.min": "Email field must be at least 3 characters !",
+              "string.max": "Email field must be a maximum of 100 characters !",
+              "string.required": "Email field is required !",
+              "string.email": "Must be a valid email!",
+            }),
+          password: joi.string().min(6).max(36).required().messages({
+            "string.base": "Password field must be valid text !",
+            "string.empty": "Password field cannot be empty !",
+            "string.min": "Password field must be at least 6 characters !",
+            "string.max": "Password field must be a maximum of 36 characters !",
+            "string.required": "Password field is required !",
+          }),
+        })
+        .validateAsync(req.body);
+    } catch (error) {
+      if (error.details && error?.details[0].message) {
+        throw new APIError(error.details[0].message, 400);
+      } else {
+        throw new APIError("Please check validation !", 400);
+      }
+    }
+    next();
+  };
 }
 
 module.exports = authValidation;
