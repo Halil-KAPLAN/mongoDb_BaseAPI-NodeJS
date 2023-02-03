f4ef887032ba22d46a9836274c3ed6a3c91566b6
Halil KAPLAN <halil-kaplan@windowslive.com>
2023-02-04 01:26:47 +0300

    url access permission with cors

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require("express-async-errors")
+require("express-async-errors");
 const express = require("express");
 const app = express();
 require("dotenv").config();
@@ -6,13 +6,16 @@ require("./src/db/dbConnection");
 const port = process.env.PORT || 5001;
 const router = require("./src/routers");
 const errorHandlerMiddleware = require("./src/middlewares/errorHandlers");
+const cors = require("cors");
+const corsOptions = require("./src/helpers/corsOptions");
 
 //Middlewares
-//app.use(express.json());
+app.use(express.json());
 app.use(express.json({ limit: "50mb" }));
 app.use(
   express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 })
 );
+app.use(cors(corsOptions));
 
 app.use("/api", router);
 
